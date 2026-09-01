7ef2d3c1eaa24daf1884d04ab69cd377a8a7b45b
arut3n <arut3ncodes@gmail.com>
2026-09-01 11:56:50 +0200

    Spring project init

---
+++ src/main/java/org/example/tutorialappspringbe/TutorialAppSpringBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.tutorialappspringbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAppSpringBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialAppSpringBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tutorialappspringbe/TutorialAppSpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tutorialappspringbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAppSpringBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
